1c6365fe12e8c6e5819a1dcd739be0921d2a3ffe
eddanieles <ed.danieles@yahoo.com>
2016-09-19 14:56:24 -0400

    updated routes

--- src/config/base.js
+++ src/config/base.js
@@ -1,1 +1,9 @@
+import Rebase from 're-base';
 
+const base = Rebase.createClass({
+   apiKey: "AIzaSyDnyYxjUl-gL7FPsEqF02xrVGDaeziFimk",
+   authDomain: "budgetapp-fc894.firebaseapp.com",
+   databaseURL: "https://budgetapp-fc894.firebaseio.com",
+})
+
+export default base;
