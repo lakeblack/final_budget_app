0af8eee9ae0cb6804b35dcb1e34163661402dede
eddanieles <ed.danieles@yahoo.com>
2016-09-27 13:54:44 -0400

    added Food.js to expenseCatergories

---
+++ src/expenseCategories/Food.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import base from '../config/base'
+
+class Food extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subs: [
+        {name: 'Groceries',
+        value: 0},
+        {name: 'Going Out',
+        value: 10}],
+      total: 10
+    }
+  }
+  handleChange(expense, index, event){
+    let newArray = this.state.subs.map((item, index) => {
+        if (item.name === expense.name){
+          return {name: expense.name, value: parseInt(event.target.value, 10)};
+        } else {
+          return item;
+        }
+      }
+    );
+    let currentTotal = newArray.map((sub, index) => sub.value).reduce((total, current) => total + current);
+    this.setState({subs: newArray, total: currentTotal });
+    // console.log(this.state.categories[index].value);
+  }
+  componentDidMount() {
+      this.ref = base.syncState(`${localStorage.UID}/myFood`, {
+        context: this,
+        state: 'subs',
+        asArray: true
+      });
+      this.ref2 = base.syncState(`${localStorage.UID}/myExpenses/1/1`, {
+        context: this,
+        state: 'total'
+      });
+   }
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
+    base.removeBinding(this.ref2);
+  }
+  render () {
+    return (
+      <div>
+        {this.state.subs.map((expense, index) =>
+          <div className="expenses" key={index}>
+            <p>
+              <span>{expense.name}:</span>
+              <input type='range' value={expense.value} min={0} max={1000} step={5} onChange={this.handleChange.bind(this, expense, index)}/>
+              <span>{expense.value}</span>
+            </p>
+          </div>
+        )}
+        <p>Food: {this.state.total}</p>
+      </div>
+    )
+  }
+}
+
+export default Food;
