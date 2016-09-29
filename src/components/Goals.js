31fca7a740a1c563a3ff0a5a50455d449ddf22cb
eddanieles <ed.danieles@yahoo.com>
2016-09-29 11:25:37 -0400

    scratch work

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -22,14 +22,14 @@ class Goals extends Component{
       });
   }
   handleStart(e){
-    console.log(this.refs.start.value);
+    //console.log(this.refs.start.value);
     let startDate = this.refs.start.value;
     this.setState({
       start: startDate,
     })
   }
   handleEnd(e){
-    console.log(this.refs.end.value);
+    //console.log(this.refs.end.value);
     let endDate = this.refs.end.value;
     this.setState({
       end: endDate,
@@ -46,17 +46,20 @@ class Goals extends Component{
 
     })
   }
+  generateChart(event){
+    event.preventDefault();
+    for (let i = parseInt(moment(this.state.start).format('M')); i < parseInt(moment(this.state.end).format('M')); i++){
+      console.log(moment(i-1).format('M'));
+    }
+    // console.log("start " + moment(this.state.start).format('MMM'));
+    //console.log(parseInt(moment(this.state.start).format('M')));
+    // console.log("end " + moment(this.state.end).format('MMM'));
+    //console.log(parseInt(moment(this.state.end).format('M')));
+  }
   render(){
     let dailySavings = this.state.goal / this.state.days
     let weeklySavings = this.state.goal / this.state.weeks
     let monthlySavings = this.state.goal / this.state.months
-    console.log(dailySavings);
-    console.log(weeklySavings);
-    console.log(monthlySavings);
-
-// console.log(range.diff('months'));
-// console.log(range.diff('weeks'));
-// console.log(range.diff('days'));
 
     return(
       <div>
@@ -64,6 +67,7 @@ class Goals extends Component{
           <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <input type="date" ref="start" onChange={this.handleStart.bind(this)}/>
             <input type="date" ref="end" onChange={this.handleEnd.bind(this)}/>
+            <button onClick={this.generateChart.bind(this)}>Generate Chart</button>
             <p>{this.state.start}</p>
             <p>{this.state.end}</p>
             <div>
