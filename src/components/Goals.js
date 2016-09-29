70ae7c0b0f5461e415944f45bdb8d7744932572d
lakeblack <lake.black@ymail.com>
2016-09-29 10:10:32 -0400

    goals page

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,15 +1,85 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard'
 import TotalExpenses from './TotalExpenses'
+import moment from 'moment';
+import range from 'moment-range'
 
 class Goals extends Component{
+  constructor(){
+    super()
+    this.state ={
+      goal: 5000,
+      start: "",
+      end: "",
+      days: "",
+      weeks: "",
+      months: "",
+    }
+  }
+  handleChange(event) {
+      this.setState({
+          goal: parseInt(event.target.value, 10)
+      });
+  }
+  handleStart(e){
+    console.log(this.refs.start.value);
+    let startDate = this.refs.start.value;
+    this.setState({
+      start: startDate,
+    })
+  }
+  handleEnd(e){
+    console.log(this.refs.end.value);
+    let endDate = this.refs.end.value;
+    this.setState({
+      end: endDate,
+    })
+  }
+  handleTime(e){
+    var start = moment(this.state.start, "YYYY-MM-DD");
+    var end   = moment(this.state.end, "YYYY-MM-DD");
+    var range = moment.range(start, end);
+    this.setState({
+      days: range.diff('days'),
+      weeks: range.diff('weeks'),
+      months: range.diff('months'),
+
+    })
+  }
   render(){
+    let dailySavings = this.state.goal / this.state.days
+    let weeklySavings = this.state.goal / this.state.weeks
+    let monthlySavings = this.state.goal / this.state.months
+    console.log(dailySavings);
+    console.log(weeklySavings);
+    console.log(monthlySavings);
+
+// console.log(range.diff('months'));
+// console.log(range.diff('weeks'));
+// console.log(range.diff('days'));
+
     return(
       <div>
         <Dashboard />
           <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <input type="date" ref="start" onChange={this.handleStart.bind(this)}/>
+            <input type="date" ref="end" onChange={this.handleEnd.bind(this)}/>
+            <p>{this.state.start}</p>
+            <p>{this.state.end}</p>
+            <div>
+              <p>Savings Goal</p>
+              <label>${this.state.goal}</label>
+              <i className="glyphicon glyphicon-usd"></i>
+            </div>
+            <input id="toggle" type='range' min={0} max={9999} step={5} value={this.state.goal} ref="goal" onChange={this.handleChange.bind(this)}/>
+            <button onClick={this.handleTime.bind(this)}>time left</button>
+
+            <p>days:{this.state.days} { dailySavings === Infinity ? 0 : "$" + Math.floor(dailySavings)}</p>
+            <p>weeks:{this.state.weeks} { weeklySavings === Infinity ? 0 : "$" + Math.floor(weeklySavings)}</p>
+            <p>months:{this.state.months} { monthlySavings === Infinity ? 0 : "$" + Math.floor(monthlySavings)}</p>
 
           </div>
+
 
       </div>
     )
